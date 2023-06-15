e58dc3c087e956c19f696f7e261c00636515d4d7
배용현 <byh9811@naver.com>
2023-06-15 23:06:27 +0900

    Initial commit

---
+++ src/test/java/com/rualone/app/AreYouAloneBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rualone.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AreYouAloneBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
